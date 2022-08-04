97b2f8d85b5d169200bb7b3beb31964ae219b71a
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-08-03 22:08:32 -0400

    Time: 111 ms (25.61%), Space: 44.4 MB (44.30%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -9,14 +9,20 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    let reverse = null
-    let next = null
-    while(head !== null){
-        next = head.next
-        head.next = reverse
-        reverse = head
-        head = next
-    }
-    return reverse
+// var reverseList = function(head) {
+//     let reverse = null
+//     let next = null
+//     while(head !== null){
+//         next = head.next
+//         head.next = reverse
+//         reverse = head
+//         head = next
+//     }
+//     return reverse
+// };
+const reverseList = (head, prev = null) => {
+  if (head === null) return prev;
+  const next = head.next;
+  head.next = prev;
+  return reverseList(next, head);
 };

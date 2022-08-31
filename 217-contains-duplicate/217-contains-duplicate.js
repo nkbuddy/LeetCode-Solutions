2923bd1dde7f50d3aac7a724e3a39c0985957d7b
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-08-31 13:21:46 -0400

    Time: 148 ms (42.71%), Space: 51.2 MB (38.33%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,5 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return !(nums.length === new Set(nums).size)
+    let seen = {}
+    for (let i = 0; i < nums.length; i++){
+        if (seen[nums[i]]) return true
+        seen[nums[i]] = true
+    }
+    return false
 };

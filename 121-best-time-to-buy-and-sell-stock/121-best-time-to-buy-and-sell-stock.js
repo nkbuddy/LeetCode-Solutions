096f4be04b7d0bea9bc388c185a4423171e2ba88
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-09-14 13:47:44 -0400

    Time: 156 ms (17.83%), Space: 51.8 MB (37.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,10 +4,10 @@
  */
 var maxProfit = function(prices) {
     let profit = 0
-    let price = prices[0]
+    let lowest = prices[0]
     for (let i = 1; i < prices.length; i++){
-        if (prices[i] < price) {[price = prices[i]]}
-        if (profit < prices[i] - price) {profit = prices[i] - price}
+        if (lowest > prices[i]){lowest = prices[i]}
+        if (profit < prices[i] - lowest) {profit = prices[i] - lowest}
     }
     return profit
 };

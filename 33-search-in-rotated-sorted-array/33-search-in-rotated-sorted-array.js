6ffc8198d7ec85ef5c0c1ad92c48353c9a040bb0
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-09-21 18:26:24 -0400

    Time: 95 ms (47.76%), Space: 42.2 MB (41.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,25 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let leftIdx = 0
-    let rightIdx = nums.length - 1
-    
-    while(leftIdx <= rightIdx){
-        
-        let middleIdx = Math.floor((rightIdx + leftIdx) / 2)
-        let middleInt = nums[middleIdx]
- 
-          if (middleInt === target) return middleIdx
-     
-        if (nums[leftIdx] <= middleInt){
-            if (nums[leftIdx]  <= target && target <= middleInt) rightIdx = middleIdx - 1
-            else leftIdx = middleIdx + 1
+    let left = 0
+    let right = nums.length - 1
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target) return mid
+        if (nums[left] <= nums[mid]){
+            if (nums[mid] < target || target < nums[left]){left = mid + 1}
+            else right = mid - 1
         }
         else {
-            if(middleInt <= target && target <= nums[rightIdx] ) leftIdx = middleIdx + 1
-            else rightIdx = middleIdx - 1
-        }
-  
+            if (nums[mid] > target || nums[right] < target){right = mid - 1}
+            else {left = mid + 1}
+    }
     }
     return -1
 };

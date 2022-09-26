e44388d65fecd13576854d5bf5495fc02ead01a2
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-09-26 15:01:53 -0400

    Time: 53 ms (99.11%), Space: 42.3 MB (19.97%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -3,10 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let split = s.split(' ')
-    result = split.pop().length 
-    while (result === 0){
-         result = split.pop().length
-    }
-    return result
+    return s.trim().split(' ').pop().length
+   
 };

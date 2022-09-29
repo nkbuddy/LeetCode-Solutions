71a242244966dc63f90f8b9e8825dfdc6aa2d50e
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-09-29 10:18:21 -0400

    Time: 121 ms (16.23%), Space: 42.7 MB (79.18%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,11 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let l = 0
-    let r = numbers.length - 1
-    while (l < r){
-        if (numbers[l] + numbers[r] < target){l++}
-        else if (numbers[l] + numbers[r] > target){r--}
-        else {return [l + 1, r + 1]}
+    let seen = {}
+    for (let i = 0; i < numbers.length; i++){
+        let diff =  target - numbers[i]
+        if (seen[diff] !== undefined){return [seen[diff], i + 1]}
+        seen[numbers[i]] = i + 1
     }
 };

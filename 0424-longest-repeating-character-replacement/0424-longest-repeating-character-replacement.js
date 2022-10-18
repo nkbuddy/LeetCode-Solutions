c46c623cfcd611714b6950821dbe0eb33d3b8ac9
Conniec321 <63067840+Conniec321@users.noreply.github.com>
2022-10-18 16:09:38 -0400

    Time: 2304 ms (5.01%), Space: 51.8 MB (5.05%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let longest = 0
+    let map = {}
+    let l = 0
+    let r = 0
+    let max  = 0
+    while(r < s.length){
+        map[s[r]] = map[s[r]] + 1 || 1
+        max = Math.max(max, map[s[r]])
+        let len = r - l + 1
+        console.log(Math.max(...Object.values(map)),map,len,s[r])
+        if ( len - max > k){
+             map[s[l]]--
+            l++
+        }
+            longest = Math.max(r - l + 1, longest)
+            r++
+        
+    }
+    return longest
+};
